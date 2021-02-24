3c8c07c300c92715e1aa8e4e73e730b077d868f8
bomi <g05050@naver.com>
2021-02-24 19:11:01 +0900

    브루트포스 15649

---
+++ bf/15649.py
@@ -0,0 +1,25 @@
+n,m =map(int, input().split())
+
+num_list=[1+ i for i in range(n)]
+check_num=[False]*n
+
+array=[]
+
+def dfs(x):
+    if x==m:
+        print(*array)
+        return
+
+    for i in range(n):
+        if check_num[i]:
+            continue
+
+        array.append(num_list[i])
+        check_num[i]=True
+
+        dfs(x+1)
+
+        array.pop()
+        check_num[i]=False
+
+dfs(0)
