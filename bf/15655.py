a7e6c8a44794590d7986d961ff623ce0222c6fee
bomi <g05050@naver.com>
2021-03-03 18:35:17 +0900

    브루트포스 15655

--- bf/15655.py
+++ bf/15655.py
@@ -1,23 +1,21 @@
 n,m =map(int, input().split())
-
 L=list(map(int, input().split()))
-
 L.sort()
 check=[False]*n
 array=[]
 
-def dfs(x):
+def dfs(x,next):
 
     if(x==m):
         print(*array)
         return
     
-    for i in range (n):
+    for i in range (next,n):
         if not check[i]:
             check[i]=True
             array.append(L[i])
-            dfs(x+1)
+            dfs(x+1,i+1)
             array.pop()
             check[i]=False
 
-dfs(0)
+dfs(0,0)
