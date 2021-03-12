370f43129d4316650bbb3cbeda175df5a44507a7
bomi <g05050@naver.com>
2021-03-12 21:41:28 +0900

    브루트포스 2529

--- bf/15661.py
+++ bf/15661.py
@@ -1,29 +1,44 @@
-#https://hiruby.tistory.com/64
-n=int(input())
-board=[list(map(int,input().split())) for _ in range(n)]
-minv=10000
+# intertools 패키지로 조합 구하기 위함
+from itertools import combinations
+import sys
 
+# 멤버들의 index 리스트화
+n = int(sys.stdin.readline())
+member_lst = list(i for i in range(n))
 
-def dfs(t,d,k):
-    global minv
-    if d==n:
-        return
-    if t==k:
-        start=link=0
-        for i in range(n):
-            for j in range(n):
-                if team[i]==True and team[j]==True:
-                    start+=board[i][j]
-                elif team[i]==False and team[j]==False:
-                    link+=board[i][j]
-        minv=min(abs(start-link),minv)
-    else:
-        team[d]=True
-        dfs(t+1,d+1,k)
-        team[d]=False
-        dfs(t,d+1,k)
-        
-for k in range(1,int(n//2)+2): #주의할 부분 파이썬은 -1부분부터
-    team = [False] * (n + 1)
-    dfs(0,0,k)
-print(minv)
+# 점수 매트릭스 만들기
+score_map = []
+for _ in range(n) :
+    score_map.append(list(map(int, sys.stdin.readline().split())))
+
+# 팀으로 나누기 위한 조합을 구함 전체 멤버에서 반으로 가를 수 있는 리스트
+
+ans = 10**8
+
+for i in range(1, int((n/2)+1)) :
+    member_divide = combinations(member_lst, i)
+    min_diff = 10**8
+
+    for x in member_divide :
+        start_lst = list(x)
+        link_lst = list(set(member_lst) - set(start_lst))
+        start_all_sum = 0
+        link_all_sum = 0
+        for j in range(n-1):
+            for k in range(n-1):
+                try :
+                    start_sum = score_map[start_lst[j]][start_lst[k]]
+                except IndexError:
+                    start_sum = 0
+                try :
+                    link_sum = score_map[link_lst[j]][link_lst[k]]
+                except IndexError:
+                    link_sum = 0
+                start_all_sum += start_sum
+                link_all_sum += link_sum
+
+        diff = abs(start_all_sum - link_all_sum)
+        min_diff = min(min_diff, diff)
+    ans = min(ans, min_diff)
+
+print(ans)
---
+++ bf/2529.py
@@ -0,0 +1,33 @@
+#https://hjp845.tistory.com/128
+#조건을 분류해주는 함수를 만드는 습관을 들여야 겠다. 
+
+num = int(input())
+op = input().split()
+check = [False] * 10
+mx , mn = "",""
+
+def poss(i,j,k): # 부등호 조건이  만족할 때만 작동
+    if k == ">":
+        return i>j
+    else:
+        return i<j
+
+
+def recu(cnt, s):
+    global mx,mn
+    if cnt > num: #맨처음 나타나는 값이 최소, 마지막 저장되는 것이 최대
+        if len(mn) == 0:
+            mn = s
+        else:
+            mx = s
+        return
+    for i in range(10): #재귀 함수
+        if check[i] == False:
+            if cnt == 0 or poss(s[-1],str(i),op[cnt-1]):
+                check[i] = True
+                recu(cnt+1,s+str(i))
+                check[i] = False
+
+recu(0,"")
+print(mx)
+print(mn)
